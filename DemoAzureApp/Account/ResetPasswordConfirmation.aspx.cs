12a2565a12752655583a0d6650bc48f6de0ed8fc
Admin <Admin@LENOVO>
2017-06-08 14:38:43 +0530

    Add project files.

---
+++ DemoAzureApp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DemoAzureApp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DemoAzureApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoAzureApp.Startup))]
+namespace DemoAzureApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
